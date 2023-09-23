f1aba13af2757970c73bf1a7243671733bff505c
manishchh <81131446+manishchh@users.noreply.github.com>
2023-09-23 19:01:17 +0930

    Added region vm for data related to region and data annotaions

---
+++ Assig1/ViewModels/RegionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Assig1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assig1.ViewModels
+{
+    public class RegionViewModel
+    {
+        [Display(Name = "Region")]
+        public int? SelectedRegionId { get; set; }
+
+        [StringLength(100, ErrorMessage = "The country name cannot exceed 80 characters.")]
+        [Display(Name = "Country Name")]
+        public string? SearchText { get; set; }
+
+        public List<Region> Regions { get; set; }
+    }
+}
