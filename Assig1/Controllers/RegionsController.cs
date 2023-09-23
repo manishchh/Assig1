bd09a485d5cc46a6a50ec0979e7a4d483ad6ab98
manishchh <81131446+manishchh@users.noreply.github.com>
2023-09-23 19:12:35 +0930

    Query for retrieving regions by filter based selected region, user search from region vm

--- Assig1/Controllers/RegionsController.cs
+++ Assig1/Controllers/RegionsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Assig1.Data;
 using Assig1.Models;
+using Assig1.ViewModels;
 
 namespace Assig1.Controllers
 {
@@ -20,18 +21,40 @@ namespace Assig1.Controllers
         }
 
         // GET: Regions
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(RegionViewModel vm)
         {
             // This is the only ViewBag you can use to set the active Menu Item.
-            ViewBag.Active = "Regions"; 
+            ViewBag.Active = "Regions";
 
-            var regions = await _context.Regions
+            IQueryable<Region> query = _context.Regions;
+
+            // filter by selected region
+            if (vm.SelectedRegionId.HasValue)
+            {
+                query = query
+                    .Where(r => r.RegionId == vm.SelectedRegionId);
+            }
+
+            // Filter by search text
+            if (!string.IsNullOrEmpty(vm.SearchText))
+            {
+                query = query
+                    .Where(r => r.RegionName
+                    .Contains(vm.SearchText, StringComparison.OrdinalIgnoreCase));
+            }
+
+            vm.Regions = await query
                 .OrderBy(r => r.RegionName)
                 .ToListAsync();
 
-            return _context.Regions != null ? 
-                          View(await _context.Regions.ToListAsync()) :
-                          Problem("Entity set 'EnvDataContext.Regions'  is null.");
+            return View(vm);
+            //var regions = await _context.Regions
+            //    .OrderBy(r => r.RegionName)
+            //    .ToListAsync();
+
+            //return _context.Regions != null ? 
+            //              View(await _context.Regions.ToListAsync()) :
+            //              Problem("Entity set 'EnvDataContext.Regions'  is null.");
         }
 
         // GET: Regions/Details/5
