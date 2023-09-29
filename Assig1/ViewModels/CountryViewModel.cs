2aa3f807518ef5d747419f781721e52782af2a56
manishchh <81131446+manishchh@users.noreply.github.com>
2023-09-29 13:23:33 +0930

    Renamed Viewmodel, deleted the original property of Regionviewmodel and added new property

---
+++ Assig1/ViewModels/CountryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Assig1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assig1.ViewModels
+{
+    public class CountryViewModel
+    {
+        public int CountryId { get; set; }
+        public string? CountryName { get; set; }
+        public string? RegionName { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
