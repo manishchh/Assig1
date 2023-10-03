d76576cf519e8492abd298fcb2981800bebb0a78
manishchh <81131446+manishchh@users.noreply.github.com>
2023-10-03 23:02:12 +1030

    Implemented year-based filtering for air quality data table

--- Assig1/wwwroot/js/site.js
+++ Assig1/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+ 
+document.getElementById("yearSelect").addEventListener("change", function (e) {
+    const selectedYear = e.target.value;
+    document.querySelectorAll("tr[data-year]").forEach(row => {
+        if (row.getAttribute("data-year") === selectedYear) {
+            row.style.display = "table-row";
+        } else {
+            row.style.display = "none";
+        }
+    });
+});
