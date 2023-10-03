280d2bbbfeb6d4d4a49fea17b5877bfc559afbc4
manishchh <81131446+manishchh@users.noreply.github.com>
2023-10-03 21:48:32 +1030

    viewmodel for city view

---
+++ Assig1/ViewModels/CityDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Assig1.ViewModels
+{
+    public class CityDetailViewModel
+    {
+        public int CityId { get; set; } 
+        public string? CityName { get; set; }
+        public int EarliestYear { get; set; }
+        public int LatestYear { get; set; }
+        public int AirQualityDataCount { get; set; }
+        
+    }
+}
---
+++ Assig1/ViewModels/CityViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Assig1.ViewModels
+{
+    public class CityViewModel
+    {
+       
+        public int CountryId { get; set; }
+        public string? CountryName { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? RegionName { get; set; }
+        public List<CityDetailViewModel> Cities { get; set; }
+    }
+}
