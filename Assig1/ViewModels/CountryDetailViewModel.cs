f0e3b2f24bb58206d344d526ab44250cca68d0a7
manishchh <81131446+manishchh@users.noreply.github.com>
2023-10-03 21:48:00 +1030

    null property check

--- Assig1/ViewModels/CountryDetailViewModel.cs
+++ Assig1/ViewModels/CountryDetailViewModel.cs
@@ -3,9 +3,9 @@
     public class CountryDetailViewModel
     {
         public int CountryId { get; set; }
-        public string CountryName { get; set; }
-        public string ImageUrl { get; set; }
-        public string RegionName { get; set; }
+        public string? CountryName { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? RegionName { get; set; }
         public List<CountryYearlyEmissions> CountryYearlyEmissions { get; set; }  
         public List<CountryYearlyTemperature> CountryYearlyTemperature { get; set; }  
     }
